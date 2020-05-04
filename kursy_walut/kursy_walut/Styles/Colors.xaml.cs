7b0d7a9570b2c670ac055a29591c486d87fa4dbd
PrzemyslawRomanczyk <49125731+PrzemyslawRomanczyk@users.noreply.github.com>
2020-05-04 13:28:51 +0200

    Dodaj pliki projektów.

---
+++ kursy_walut/kursy_walut/Styles/Colors.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kursy_walut.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Colors : ContentPage
+    {
+        public Colors()
+        {
+        }
+    }
+}
---
+++ kursy_walut/kursy_walut/Views/HomePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace kursy_walut
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+        }
+    }
+}
