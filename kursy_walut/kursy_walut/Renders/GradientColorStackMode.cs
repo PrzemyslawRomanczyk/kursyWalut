4b93c6fe4b3b430e1551957790747cfb9933a999
PrzemyslawRomanczyk <49125731+PrzemyslawRomanczyk@users.noreply.github.com>
2020-06-15 18:42:41 +0200

    Logo adjusment + bacground gradient

---
+++ kursy_walut/kursy_walut/Renders/GradientColorStackMode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kursy_walut.Renders
+{
+    public enum GradientColorStackMode
+    {
+        ToRight,
+        ToLeft,
+        ToTop,
+        ToBottom,
+        ToTopLeft,
+        ToTopRight,
+        ToBottomLeft,
+        ToBottomRight
+    }
+}
